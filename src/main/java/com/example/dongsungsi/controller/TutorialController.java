0af1f80d27f2a15bdf81100fdf090d18768f4916
Yu Jong Cheul <51194504+whdcjf96@users.noreply.github.com>
2022-05-26 12:22:59 +0900

    백엔드 완성
    crud 기능 추가완

--- src/main/java/com/example/dongsungsi/controller/TutorialController.java
+++ src/main/java/com/example/dongsungsi/controller/TutorialController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
 
@@ -28,7 +29,7 @@ public class TutorialController {
     @Autowired
     TutorialServiceImpl tutorialService;
 
-
+//  insert
     @PostMapping("/tutorials")
     public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial){
         boolean bSuccess =tutorialService.save(tutorial);
@@ -44,6 +45,7 @@ public class TutorialController {
         }
     }
 
+//    id select
     @GetMapping("/tutorials/{id}")
     public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") Long id){
         Optional<Tutorial> tutorial=tutorialService.findbyId(id);
@@ -56,4 +58,94 @@ public class TutorialController {
         }
 
     }
+
+//    title select
+    @GetMapping("/tutorials")
+    public ResponseEntity<List<Tutorial>> getTitleTutorials(@RequestParam(required = false) String title){
+//        title(제목)을 조회하는 서비스를 호출
+        List<Tutorial> tutorials = tutorialService.findByTitleContaining(title);
+        try{
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(tutorials,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+//    update : @PutMapping
+//    @PathVariable : url의 {}부분
+//    @RequestBody : json 객체형태로 데이터 받기
+    @PutMapping("/tutorials/{id}")
+    public ResponseEntity<Tutorial> updateTutorial(@PathVariable("id") Long id, @RequestBody Tutorial tutorial){
+//        id에 해당하는 데이터가 있는지 조회
+//        Optional<객체> : 객체가 null이면 ""자동으로 바꿈(null포인트에러 방지)
+        Optional<Tutorial> tutorialData= tutorialService.findbyId(id);
+
+        if(tutorialData.isPresent()){
+//            tutorialData 가 있으면 save호출( id가 있으면 update)
+            boolean bSuccess = tutorialService.save(tutorial);
+
+            if (bSuccess == true) {
+//                save 호출 성공하면
+                return new ResponseEntity<>(tutorial,HttpStatus.CREATED);
+            }
+//            save 호출 실패하면
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }else {
+//            tutorialData가 없으면
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+//    delete
+    @PutMapping("/tutorials/deletion/{id}")
+    public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable("id") Long id){
+        boolean bSuccess = tutorialService.deleteById(id);
+
+//        프론트엔드 쪽으로 상태정보를 보내줌
+        try{
+            if (bSuccess == true) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+//            update문이 실패했을 경우(0건 수정이 될 경우)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+//    delete all
+    @PutMapping("/tutorials")
+    public ResponseEntity<HttpStatus> deleteAllTutorials(){
+        boolean Bsuccess = tutorialService.deleteAll();
+
+        try {
+            if (Bsuccess == true){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/tutorials/published")
+    public ResponseEntity<List<Tutorial>> findbyPublished(){
+        List<Tutorial> tutorials = tutorialService.findByPublished("Y");
+
+        try{
+            if (tutorials.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(tutorials,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+    }
+
 }
